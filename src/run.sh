c1f8cdffc635538c7a4e714f260b85ba5b91fddb
PlayerG9 <68517540+PlayerG9@users.noreply.github.com>
2022-08-26 09:53:39 +0200

    fixed run script

--- src/run.sh
+++ src/run.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-SRCDIR=$(dirname "$0")
-
-"$SRCDIR/../.venv/bin/python3" -B -O "SRCDIR/git-runner/main.py"
+"$(dirname "$0")/../.venv/bin/python3" -B -O "$(dirname "$0")/git-runner/main.py"
