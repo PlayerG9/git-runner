afb5c98c0628d5f7402fb290cb5fd8d6b23ae9a1
PlayerG9 <68517540+PlayerG9@users.noreply.github.com>
2022-08-31 17:01:23 +0200

    removed CustomScheduler because it has flaws (infinite starting of the job)

--- src/git-runner/pyconfig/config_schedule.py
+++ src/git-runner/pyconfig/config_schedule.py
@@ -9,15 +9,6 @@ from _thread import start_new_thread
 import schedule
 
 
-class CustomScheduler(schedule.Scheduler):
-    def _run_job(self, job: "schedule.Job") -> None:
-        logging.debug(f"running job {job.job_func.func.__name__}")
-        start_new_thread(job.run, ())
-
-
-schedule.default_scheduler = CustomScheduler()
-
-
 @(lambda fn: start_new_thread(fn, ()))
 def scheduler():
     while True:
