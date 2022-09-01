30bb607d04c2ef0d67ed4997f7d59718ee080875
PlayerG9 <68517540+PlayerG9@users.noreply.github.com>
2022-09-01 21:26:35 +0200

    support for frontend-based routing (e.g. react-router)

--- src/git-runner/webinterface/__init__.py
+++ src/git-runner/webinterface/__init__.py
@@ -5,6 +5,7 @@ r"""
 """
 import os.path
 import fastapi
+from fastapi.responses import FileResponse
 
 
 website = fastapi.APIRouter()
@@ -22,7 +23,7 @@ RESOURCE_PATH = os.path.abspath(
     include_in_schema=False
 )
 async def getIndex():
-    return fastapi.responses.FileResponse(webResource('index.html'))
+    return FileResponse(webResource('index.html'))
 
 
 @website.get(
@@ -30,7 +31,9 @@ async def getIndex():
     include_in_schema=False
 )
 async def getFile(path: str = fastapi.Path()):
-    return fastapi.responses.FileResponse(webResource(path))
+    if os.path.splitext(path)[1]:
+        return FileResponse(webResource('index.html'))  # support for front-end based routing (e.g. react-router)
+    return FileResponse(webResource(path))
 
 
 def webResource(path: str):
