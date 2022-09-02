3011b9bee8fc5972df1d8ffdc4bd52cb56e36ab1
PlayerG9 <68517540+PlayerG9@users.noreply.github.com>
2022-09-02 14:40:22 +0200

    added CORS-Middleware and configured FastAPI creation

--- src/git-runner/app.py
+++ src/git-runner/app.py
@@ -3,14 +3,24 @@
 r"""
 
 """
-import fastapi
 import pyconfig  # noqa (configures python and libraries)
+import fastapi
+import fastapi.middleware.cors as cors
+# import fastapi.middleware.gzip as gzip
 from api import api as api_router
 from webinterface import website as website_router
 import update_checker  # noqa
 import database
 
-app = fastapi.FastAPI()
+__version__ = (0, 1, 0)
+__version_str__ = '.'.join(str(v) for v in __version__)
+
+app = fastapi.FastAPI(
+    title="Git-Runner",
+    version=__version_str__,
+)
+app.add_middleware(cors.CORSMiddleware)
+# app.add_middleware(gzip.GZipMiddleware)
 
 app.include_router(api_router)
 app.include_router(website_router)
