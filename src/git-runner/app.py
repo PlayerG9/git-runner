5529be66a618f87a5abb5ece8fe1de0886080dd0
PlayerG9 <g9yout@gmail.com>
2022-09-05 17:39:54 +0200

    completely rewritten WSGI-App

--- src/git-runner/app.py
+++ src/git-runner/app.py
@@ -5,24 +5,30 @@ r"""
 """
 import pyconfig  # noqa (configures python and libraries)
 import fastapi
-import fastapi.middleware.cors as cors
-# import fastapi.middleware.gzip as gzip
-from api import api as api_router
-from webinterface import website as website_router
-import update_checker  # noqa
-import database
+from fastapi.staticfiles import StaticFiles
 
-__version__ = (0, 1, 0)
-__version_str__ = '.'.join(str(v) for v in __version__)
+import utility
+from api import api as api_app
+
 
 app = fastapi.FastAPI(
-    title="Git-Runner",
-    version=__version_str__,
+    openapi_url=None,
+    docs_url=None,
+    redoc_url=None
 )
-app.add_middleware(cors.CORSMiddleware)
-# app.add_middleware(gzip.GZipMiddleware)
 
-app.include_router(api_router)
-app.include_router(website_router)
 
-database.initialiseDatabase()
+app.mount('/api', api_app)
+app.mount('/', StaticFiles(directory=utility.local('website'), html=True))
+
+
+@app.exception_handler(404)
+async def custom404handler(request: fastapi.Request, exception: Exception):
+    file_handler: StaticFiles = request.scope.get('endpoint', None)
+    if isinstance(file_handler, StaticFiles):
+        # github-pages like 404.html option
+        custom404file = file_handler.lookup_path('404.html')[0]
+        if custom404file:
+            return file_handler.get_response(path=custom404file, scope=request.scope)
+
+    return fastapi.responses.FileResponse(utility.local('utility', '404.html'))
