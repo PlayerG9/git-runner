b918d036a0bf3c413dd76d6d4ea644e589b2d753
PlayerG9 <g9yout@gmail.com>
2022-09-13 14:55:30 +0200

    removed default port option from runner script

--- src/run.sh
+++ src/run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-"$(dirname "$0")/../.venv/bin/uvicorn" "app:app" --app-dir "$(dirname "$0")/git-runner" --host "0.0.0.0" --port 42069 "$@"
+"$(dirname "$0")/../.venv/bin/uvicorn" "app:app" --app-dir "$(dirname "$0")/git-runner" --host "0.0.0.0" "$@"
