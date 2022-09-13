cf2f39fc9cfdad258500c1363216ea4428755891
PlayerG9 <g9yout@gmail.com>
2022-09-13 14:57:47 +0200

    code reformat, endpoint for reset

--- src/git-runner/api/auth/__init__.py
+++ src/git-runner/api/auth/__init__.py
@@ -17,7 +17,10 @@ def auth(token: str = fastapi.Depends(fastapi.security.APIKeyHeader(name="api-ke
     return token
 
 
-auth_router = fastapi.APIRouter(prefix="/auth")
+auth_router = fastapi.APIRouter(
+    prefix="/auth",
+    tags=["authentication"],
+)
 
 
 @auth_router.get(
@@ -25,12 +28,26 @@ auth_router = fastapi.APIRouter(prefix="/auth")
     response_model=models.TokenResponse
 )
 async def getToken(credentials: fastapi.security.HTTPBasicCredentials = fastapi.Depends(fastapi.security.HTTPBasic())):
+    r"""
+    get the api-token for the user
+    """
     login.verify_login(username=credentials.username, password=credentials.password)
-    authentication = database.crud.getOrCreateAuth(credentials.username)
-    print(authentication.__dict__)
+    authentication = database.crud.getOrCreateAuth(username=credentials.username)
     return models.TokenResponse(
         token=authentication.auth_key
     )
+
+
+@auth_router.get(
+    '/reset',
+    response_model=models.TokenResponse
+)
+async def getReset(token: str = auth):
+    r"""
+    reset the api-token (logs out on all devices)
+    """
+    database.crud.deleteAuth(token=token)
+    return {}
 
 
 @auth_router.get(
@@ -38,6 +55,9 @@ async def getToken(credentials: fastapi.security.HTTPBasicCredentials = fastapi.
     response_model=models.VerifyResponse
 )
 async def getVerify(token: str = auth):
+    r"""
+    simply verify if the current token is valid
+    """
     return dict(
         success=True,
         token=token
